a4fb81c3686cf10f8c7d7beaf3f52bd364586e00
taehyeooo <dony527@naver.com>
2026-03-07 12:57:15 +0900

    데일리 미션 0307

--- src/main/java/com/example/daily0303/Daily0303Application.java
+++ src/main/java/com/example/daily0303/Daily0303Application.java
@@ -1,4 +1,11 @@
 package com.example.daily0303;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Daily0303Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Daily0303Application.class, args);
+    }
 }
--- src/main/java/com/example/daily0303/entity/Board.java
+++ src/main/java/com/example/daily0303/entity/Board.java
@@ -1,4 +1,29 @@
 package com.example.daily0303.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String writer;
+
+    public Board(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
 }
